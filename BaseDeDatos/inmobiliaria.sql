e37d49fed2745851641c8063e3c527a65684ea13
AdSarchioni <adsarchioni@gmail.com>
2024-11-10 21:11:11 -0300

    10

--- BaseDeDatos/inmobiliaria.sql
+++ BaseDeDatos/inmobiliaria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-10-2024 a las 20:05:40
+-- Tiempo de generación: 11-11-2024 a las 01:07:36
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -73,6 +73,7 @@ CREATE TABLE `inmueble` (
   `Patio` int(11) DEFAULT NULL,
   `Precio` double DEFAULT NULL,
   `Condicion` varchar(50) DEFAULT NULL,
+  `imagen` varchar(255) DEFAULT NULL,
   `Estado_Inmueble` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -80,27 +81,29 @@ CREATE TABLE `inmueble` (
 -- Volcado de datos para la tabla `inmueble`
 --
 
-INSERT INTO `inmueble` (`Id_Inmueble`, `Id_Propietario`, `Direccion`, `Uso`, `Ambientes`, `Latitud`, `Longitud`, `Tamano`, `Id_Tipo_Inmueble`, `Servicios`, `Bano`, `Cochera`, `Patio`, `Precio`, `Condicion`, `Estado_Inmueble`) VALUES
-(1, 1, 'Av. Principal 123', 'Residencial', 3, '-34.6037', '-58.3816', 75, 1, 'Agua, Luz, Gas', 1, 1, 1, 120000, 'Alquiler', 1),
-(2, 1, 'Calle Secundaria 456', 'Comercial', 2, '-34.6037', '-58.3816', 50, 2, 'Agua, Luz', 1, 0, 0, 80000, 'Alquiler', 0),
-(3, 2, 'Calle Tercera 789', 'Residencial', 4, '-34.6037', '-58.3816', 100, 1, 'Agua, Luz, Gas, Internet', 2, 1, 1, 150000, 'Alquiler', 1),
-(4, 2, 'Av. Cuarta 101', 'Residencial', 2, '-34.6037', '-58.3816', 60, 3, 'Agua, Luz, Gas', 1, 1, 0, 90000, 'Venta', 1),
-(5, 3, 'Calle Quinta 202', 'Comercial', 1, '-34.6037', '-58.3816', 30, 2, 'Agua, Luz', 1, 0, 0, 60000, 'Venta', 0),
-(6, 3, 'Calle Sexta 303', 'Residencial', 3, '-34.6037', '-58.3816', 80, 1, 'Agua, Luz', 1, 1, 1, 110000, 'Alquiler', 0),
-(7, 4, 'Av. Séptima 404', 'Residencial', 2, '-34.6037', '-58.3816', 70, 2, 'Agua, Luz, Gas', 1, 0, 1, 95000, 'Venta', 0),
-(8, 4, 'Calle Octava 505', 'Residencial', 3, '-34.6037', '-58.3816', 85, 1, 'Agua, Luz, Gas, Internet', 2, 1, 0, 130000, 'Venta', 1),
-(9, 5, 'Av. Novena 606', 'Comercial', 1, '-34.6037', '-58.3816', 40, 2, 'Agua, Luz', 1, 0, 0, 70000, 'Alquiler', 0),
-(10, 6, 'Calle Décima 707', 'Residencial', 4, '-34.6037', '-58.3816', 120, 1, 'Agua, Luz, Gas, Internet', 2, 1, 1, 160000, 'Alquiler', 1),
-(11, 6, 'Av. Once 808', 'Residencial', 2, '-34.6037', '-58.3816', 55, 3, 'Agua, Luz', 1, 1, 0, 85000, 'Alquiler', 0),
-(12, 7, 'Calle Doce 909', 'Residencial', 3, '-34.6037', '-58.3816', 90, 1, 'Agua, Luz', 1, 1, 1, 120000, 'Alquiler', 1),
-(13, 7, 'Av. Trece 1010', 'Comercial', 1, '-34.6037', '-58.3816', 35, 2, 'Agua, Luz', 1, 0, 0, 65000, 'Venta', 0),
-(14, 8, 'Calle Catorce 1111', 'Residencial', 3, '-34.6037', '-58.3816', 95, 1, 'Agua, Luz, Gas', 2, 1, 1, 140000, 'Alquiler', 0),
-(15, 8, 'Av. Quince 1212', 'Residencial', 2, '-34.6037', '-58.3816', 65, 2, 'Agua, Luz', 1, 1, 0, 90000, 'Venta', 0),
-(16, 9, 'Av. Decimosexta 1313', 'Comercial', 1, '-34.6037', '-58.3816', 45, 2, 'Agua, Luz', 1, 0, 0, 70000, 'Alquiler', 1),
-(17, 10, 'Calle Decimoséptima 1414', 'Residencial', 4, '-34.6037', '-58.3816', 110, 1, 'Agua, Luz, Gas', 2, 1, 1, 155000, 'Alquiler', 0),
-(18, 11, 'Av. Decimoctava 1515', 'Residencial', 2, '-34.6037', '-58.3816', 50, 3, 'Agua, Luz', 1, 1, 0, 85000, 'Alquiler', 0),
-(19, 12, 'Calle Decimonovena 1616', 'Residencial', 3, '-34.6037', '-58.3816', 80, 1, 'Agua, Luz, Gas', 2, 1, 1, 120000, 'Alquiler', 1),
-(20, 13, 'Av. Vigésima 1717', 'Comercial', 1, '-34.6037', '-58.3816', 40, 2, 'Agua, Luz', 1, 0, 0, 65000, 'Alquiler', 0);
+INSERT INTO `inmueble` (`Id_Inmueble`, `Id_Propietario`, `Direccion`, `Uso`, `Ambientes`, `Latitud`, `Longitud`, `Tamano`, `Id_Tipo_Inmueble`, `Servicios`, `Bano`, `Cochera`, `Patio`, `Precio`, `Condicion`, `imagen`, `Estado_Inmueble`) VALUES
+(1, 1, 'Av. Principal 123', 'Residencial', 3, NULL, NULL, 78, 1, 'Agua, Luz, Gas', 0, 0, 0, 120000, 'Alquiler', NULL, 0),
+(2, 1, 'Calle Secundaria 456', 'Comercial', 2, '-34.6037', '-58.3816', 50, 2, 'Agua, Luz', 1, 0, 0, 80000, 'Alquiler', 'casa2.png', 1),
+(3, 2, 'Calle Tercera 789', 'Residencial', 4, '-34.6037', '-58.3816', 100, 1, 'Agua, Luz, Gas, Internet', 2, 1, 1, 150000, 'Alquiler', 'casa5.PNG', 1),
+(4, 2, 'Av. Cuarta 101', 'Residencial', 2, '-34.6037', '-58.3816', 60, 3, 'Agua, Luz, Gas', 1, 1, 0, 90000, 'Venta', 'casa4.PNG', 1),
+(5, 3, 'Calle Quinta 202', 'Comercial', 1, '-34.6037', '-58.3816', 30, 2, 'Agua, Luz', 1, 0, 0, 60000, 'Venta', NULL, 0),
+(6, 3, 'Calle Sexta 303', 'Residencial', 3, '-34.6037', '-58.3816', 80, 1, 'Agua, Luz', 1, 1, 1, 110000, 'Alquiler', NULL, 0),
+(7, 4, 'Av. Séptima 404', 'Residencial', 2, '-34.6037', '-58.3816', 70, 2, 'Agua, Luz, Gas', 1, 0, 1, 95000, 'Venta', NULL, 0),
+(8, 4, 'Calle Octava 505', 'Residencial', 3, '-34.6037', '-58.3816', 85, 1, 'Agua, Luz, Gas, Internet', 2, 1, 0, 130000, 'Venta', NULL, 1),
+(9, 5, 'Av. Novena 606', 'Comercial', 1, '-34.6037', '-58.3816', 40, 2, 'Agua, Luz', 1, 0, 0, 70000, 'Alquiler', NULL, 0),
+(10, 6, 'Calle Décima 707', 'Residencial', 4, '-34.6037', '-58.3816', 120, 1, 'Agua, Luz, Gas, Internet', 2, 1, 1, 160000, 'Alquiler', NULL, 1),
+(11, 6, 'Av. Once 808', 'Residencial', 2, '-34.6037', '-58.3816', 55, 3, 'Agua, Luz', 1, 1, 0, 85000, 'Alquiler', NULL, 0),
+(12, 7, 'Calle Doce 909', 'Residencial', 3, '-34.6037', '-58.3816', 90, 1, 'Agua, Luz', 1, 1, 1, 120000, 'Alquiler', NULL, 1),
+(13, 7, 'Av. Trece 1010', 'Comercial', 1, '-34.6037', '-58.3816', 35, 2, 'Agua, Luz', 1, 0, 0, 65000, 'Venta', NULL, 0),
+(14, 8, 'Calle Catorce 1111', 'Residencial', 3, '-34.6037', '-58.3816', 95, 1, 'Agua, Luz, Gas', 2, 1, 1, 140000, 'Alquiler', NULL, 0),
+(15, 8, 'Av. Quince 1212', 'Residencial', 2, '-34.6037', '-58.3816', 65, 2, 'Agua, Luz', 1, 1, 0, 90000, 'Venta', NULL, 0),
+(16, 9, 'Av. Decimosexta 1313', 'Comercial', 1, '-34.6037', '-58.3816', 45, 2, 'Agua, Luz', 1, 0, 0, 70000, 'Alquiler', NULL, 1),
+(17, 10, 'Calle Decimoséptima 1414', 'Residencial', 4, '-34.6037', '-58.3816', 110, 1, 'Agua, Luz, Gas', 2, 1, 1, 155000, 'Alquiler', NULL, 0),
+(18, 11, 'Av. Decimoctava 1515', 'Residencial', 2, '-34.6037', '-58.3816', 50, 3, 'Agua, Luz', 1, 1, 0, 85000, 'Alquiler', NULL, 0),
+(19, 12, 'Calle Decimonovena 1616', 'Residencial', 3, '-34.6037', '-58.3816', 80, 1, 'Agua, Luz, Gas', 2, 1, 1, 120000, 'Alquiler', NULL, 1),
+(20, 13, 'Av. Vigésima 1717', 'Comercial', 1, '-34.6037', '-58.3816', 40, 2, 'Agua, Luz', 1, 0, 0, 65000, 'Alquiler', NULL, 0),
+(29, 1, 'Calle Falsa 123', 'Residencial', 3, '-34.603722', '-58.381592', 120.5, 2, 'Agua, Luz, Gas', 2, 1, 1, 150000, 'En Venta', NULL, 1),
+(30, 1, 'Calle Falsa 123', 'Residencial', 3, '-34.603722', '-58.381592', 120.5, 3, 'Agua, Luz, Gas', 2, 1, 1, 150000, 'En Venta', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -206,7 +209,35 @@ INSERT INTO `logs` (`Id`, `LogLevel`, `Message`, `Timestamp`, `Usuario`) VALUES
 (44, 'Information', 'El usuario ha accedido a nuevo pago de id: 26', '2024-09-20 19:59:06', 'Sarchioni,Adrian-(1)'),
 (45, 'Guardar', 'El usuario ha accedido a guardar pago anulado.', '2024-09-20 19:59:13', 'Sarchioni,Adrian-(1)'),
 (46, 'Information', 'El usuario ha accedido a nuevo pago de id: 26', '2024-09-20 20:00:28', 'Sarchioni,Adrian-(1)'),
-(47, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:1', '2024-09-20 20:00:32', 'Sarchioni,Adrian-(1)');
+(47, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:1', '2024-09-20 20:00:32', 'Sarchioni,Adrian-(1)'),
+(48, 'Information', 'El usuario ha accedido a nuevo pago de id: 27', '2024-10-29 21:29:54', 'Sarchioni,Adrian-(1)'),
+(49, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:1', '2024-10-29 21:30:00', 'Sarchioni,Adrian-(1)'),
+(50, 'Information', 'El usuario ha accedido a nuevo pago de id: 27', '2024-10-29 21:30:02', 'Sarchioni,Adrian-(1)'),
+(51, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:2', '2024-10-29 21:30:05', 'Sarchioni,Adrian-(1)'),
+(52, 'Information', 'El usuario ha accedido a nuevo pago de id: 27', '2024-10-29 21:30:07', 'Sarchioni,Adrian-(1)'),
+(53, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:3', '2024-10-29 21:30:14', 'Sarchioni,Adrian-(1)'),
+(54, 'Information', 'El usuario ha accedido a nuevo pago de id: 27', '2024-10-29 21:30:16', 'Sarchioni,Adrian-(1)'),
+(55, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:4', '2024-10-29 21:30:20', 'Sarchioni,Adrian-(1)'),
+(56, 'Information', 'El usuario ha accedido a nuevo pago de id: 27', '2024-10-29 21:30:21', 'Sarchioni,Adrian-(1)'),
+(57, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:5', '2024-10-29 21:30:25', 'Sarchioni,Adrian-(1)'),
+(58, 'Information', 'El usuario ha accedido a nuevo pago de id: 27', '2024-10-29 21:30:26', 'Sarchioni,Adrian-(1)'),
+(59, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:6', '2024-10-29 21:30:30', 'Sarchioni,Adrian-(1)'),
+(60, 'Information', 'El usuario ha accedido a nuevo pago de id: 28', '2024-10-29 21:30:43', 'Sarchioni,Adrian-(1)'),
+(61, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:1', '2024-10-29 21:30:47', 'Sarchioni,Adrian-(1)'),
+(62, 'Information', 'El usuario ha accedido a nuevo pago de id: 28', '2024-10-29 21:30:48', 'Sarchioni,Adrian-(1)'),
+(63, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:2', '2024-10-29 21:30:52', 'Sarchioni,Adrian-(1)'),
+(64, 'Information', 'El usuario ha accedido a nuevo pago de id: 28', '2024-10-29 21:30:53', 'Sarchioni,Adrian-(1)'),
+(65, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:3', '2024-10-29 21:30:57', 'Sarchioni,Adrian-(1)'),
+(66, 'Information', 'El usuario ha accedido a nuevo pago de id: 28', '2024-10-29 21:30:58', 'Sarchioni,Adrian-(1)'),
+(67, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:4', '2024-10-29 21:31:01', 'Sarchioni,Adrian-(1)'),
+(68, 'Information', 'El usuario ha accedido a nuevo pago de id: 26', '2024-10-29 21:31:05', 'Sarchioni,Adrian-(1)'),
+(69, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:1', '2024-10-29 21:31:10', 'Sarchioni,Adrian-(1)'),
+(70, 'Information', 'El usuario ha accedido a nuevo pago de id: 26', '2024-10-29 21:31:11', 'Sarchioni,Adrian-(1)'),
+(71, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:2', '2024-10-29 21:31:15', 'Sarchioni,Adrian-(1)'),
+(72, 'Information', 'El usuario ha accedido a nuevo pago de id: 26', '2024-10-29 21:31:17', 'Sarchioni,Adrian-(1)'),
+(73, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:3', '2024-10-29 21:31:20', 'Sarchioni,Adrian-(1)'),
+(74, 'Information', 'El usuario ha accedido a nuevo pago de id: 26', '2024-10-29 21:31:21', 'Sarchioni,Adrian-(1)'),
+(75, 'Guardar', 'El usuario ha accedido a guardar pago cuota paga:4', '2024-10-29 21:31:24', 'Sarchioni,Adrian-(1)');
 
 -- --------------------------------------------------------
 
@@ -233,7 +264,21 @@ CREATE TABLE `pago` (
 INSERT INTO `pago` (`Id_Pago`, `Id_Contrato`, `Importe`, `CuotaPaga`, `Fecha`, `Multa`, `Id_Creado_Por`, `Id_Terminado_Por`, `Estado_Pago`) VALUES
 (61, 26, 150000, 0, '2024-09-20', 0, 1, 1, 0),
 (62, 26, 150000, 0, '2024-09-20', 0, 1, 0, 1),
-(63, 26, 150000, 0, '2024-09-20', 0, 1, 0, 1);
+(63, 26, 150000, 0, '2024-09-20', 0, 1, 0, 1),
+(64, 27, 90000, 1, '2024-10-29', 0, 1, 0, 1),
+(65, 27, 90000, 2, '2024-10-29', 0, 1, 0, 1),
+(66, 27, 90000, 3, '2024-10-29', 0, 1, 0, 1),
+(67, 27, 90000, 4, '2024-10-29', 0, 1, 0, 1),
+(68, 27, 90000, 5, '2024-10-29', 0, 1, 0, 1),
+(69, 27, 90000, 6, '2024-10-29', 0, 1, 0, 1),
+(70, 28, 160000, 1, '2024-10-29', 0, 1, 0, 1),
+(71, 28, 160000, 2, '2024-10-29', 0, 1, 0, 1),
+(72, 28, 160000, 3, '2024-10-29', 0, 1, 0, 1),
+(73, 28, 160000, 4, '2024-10-29', 0, 1, 0, 1),
+(74, 26, 150000, 1, '2024-10-29', 0, 1, 0, 1),
+(75, 26, 150000, 2, '2024-10-29', 0, 1, 0, 1),
+(76, 26, 150000, 3, '2024-10-29', 0, 1, 0, 1),
+(77, 26, 150000, 4, '2024-10-29', 0, 1, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -250,6 +295,7 @@ CREATE TABLE `propietario` (
   `Telefono` varchar(255) DEFAULT NULL,
   `Email` varchar(255) DEFAULT NULL,
   `Clave` varchar(255) NOT NULL,
+  `Avatar` varchar(255) NOT NULL,
   `Estado_Propietario` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -257,23 +303,23 @@ CREATE TABLE `propietario` (
 -- Volcado de datos para la tabla `propietario`
 --
 
-INSERT INTO `propietario` (`Id_Propietario`, `Dni`, `Apellido`, `Nombre`, `Direccion`, `Telefono`, `Email`, `Clave`, `Estado_Propietario`) VALUES
-(1, 12345678, 'Alvarez', 'Juan Jose', 'Av. Principal 123', '9876-5431', 'juan.alvarez@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 0),
-(2, 23456789, 'Moreno', 'Laura', 'Calle Secundaria 456', '555-2222', 'laura.moreno@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 1),
-(3, 34567890, 'Ruiz', 'Miguel', 'Calle Tercera 789', '555-3333', 'miguel.ruiz@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 0),
-(4, 45678901, 'Fernández', 'Sofía', 'Av. Cuarta 101', '555-4444', 'sofia.fernandez@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 1),
-(5, 56789012, 'Jiménez', 'Ricardo Dario', 'Calle Quinta 202', '5555555', 'ricardo.jimenez@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 0),
-(6, 67890123, 'Torres', 'Ana', 'Calle Sexta 303', '555-6666', 'ana.torres@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 0),
-(7, 78901234, 'García', 'Carlos', 'Av. Séptima 404', '555-7777', 'carlos.garcia@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 0),
-(8, 89012345, 'Pérez', 'Isabel', 'Calle Octava 505', '555-8888', 'isabel.perez@example.com', '', 0),
-(9, 90123456, 'Gómez', 'Luis', 'Av. Novena 606', '555-9999', 'luis.gomez@example.com', '', 1),
-(10, 1234567, 'Castro', 'María', 'Calle Décima 707', '555-0000', 'maria.castro@example.com', '', 1),
-(11, 12345679, 'Molina', 'Pedro', 'Av. Once 808', '555-1234', 'pedro.molina@example.com', '', 0),
-(12, 23456780, 'Sánchez', 'Sandra', 'Calle Doce 909', '555-2345', 'sandra.sanchez@example.com', '', 1),
-(13, 34567801, 'Morales', 'Elena', 'Av. Trece 1010', '555-3456', 'elena.morales@example.com', '', 1),
-(14, 45678912, 'Vázquez', 'Antonio', 'Calle Catorce 1111', '555-4567', 'antonio.vazquez@example.com', '', 0),
-(15, 56789023, 'Guerrero', 'Natalia', 'Av. Quince 1212', '555-5678', 'natalia.guerrero@example.com', '', 1),
-(19, 1345552, 'gomezzz', 'Orlando Ramon', 'Av. Pringless 123', '9876-5431', 'o@o', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', 1);
+INSERT INTO `propietario` (`Id_Propietario`, `Dni`, `Apellido`, `Nombre`, `Direccion`, `Telefono`, `Email`, `Clave`, `Avatar`, `Estado_Propietario`) VALUES
+(1, 12345678, 'Alvarezzzz', 'Juan Jose', 'Av. Principal 123', '9876-5431', 'a@a', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 0),
+(2, 23456789, 'Moreno', 'Laura Andrea', 'Av. Espaa 1669 ', '555-2244', 'l@l', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 1),
+(3, 34567890, 'Ruiz', 'Miguel', 'Calle Tercera 789', '555-3333', 'miguel.ruiz@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 0),
+(4, 45678901, 'Fernández', 'Sofía', 'Av. Cuarta 101', '555-4444', 'sofia.fernandez@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 1),
+(5, 56789012, 'Jiménez', 'Ricardo Dario', 'Calle Quinta 202', '5555555', 'ricardo.jimenez@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 0),
+(6, 67890123, 'Torres', 'Ana', 'Calle Sexta 303', '555-6666', 'ana.torres@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 0),
+(7, 78901234, 'García', 'Carlos', 'Av. Séptima 404', '555-7777', 'carlos.garcia@example.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '', 0),
+(8, 89012345, 'Pérez', 'Isabel', 'Calle Octava 505', '555-8888', 'isabel.perez@example.com', '', '', 0),
+(9, 90123456, 'Gómez', 'Luis', 'Av. Novena 606', '555-9999', 'luis.gomez@example.com', '', '', 1),
+(10, 1234567, 'Castro', 'María', 'Calle Décima 707', '555-0000', 'maria.castro@example.com', '', '', 1),
+(11, 12345679, 'Molina', 'Pedro', 'Av. Once 808', '555-1234', 'pedro.molina@example.com', '', '', 0),
+(12, 23456780, 'Sánchez', 'Sandra', 'Calle Doce 909', '555-2345', 'sandra.sanchez@example.com', '', '', 1),
+(13, 34567801, 'Morales', 'Elena', 'Av. Trece 1010', '555-3456', 'elena.morales@example.com', '', '', 1),
+(14, 45678912, 'Vázquez', 'Antonio', 'Calle Catorce 1111', '555-4567', 'antonio.vazquez@example.com', '', '', 0),
+(15, 56789023, 'Guerrero', 'Natalia', 'Av. Quince 1212', '555-5678', 'natalia.guerrero@example.com', '', '', 1),
+(19, 1345552, 'gomezzzz', 'Orlando Ramon', 'Av. Pringless 1287783', '9876-5431', 'o@o', 'KDa9xkd7uC5ppAF9LVVP4wOEmmDb96jKjB8L2mDPlgA=', '', 1);
 
 -- --------------------------------------------------------
 
@@ -301,7 +347,8 @@ INSERT INTO `tipo_inmueble` (`Id_Tipo_Inmueble`, `Tipo`, `Estado_Tipo_Inmueble`)
 (7, 'Cabaña', 0),
 (8, 'Chalet', 1),
 (9, 'Penthouse', 1),
-(10, 'Garaje', 1);
+(10, 'Garaje', 1),
+(11, 'casa', 0);
 
 -- --------------------------------------------------------
 
@@ -331,6 +378,17 @@ INSERT INTO `usuario` (`Id_Usuario`, `Apellido`, `Nombre`, `Dni`, `Telefono`, `R
 (3, 'Lucero', 'Gastonn', 1223344, '1233454', 1, 'bu@bu.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '/Uploads\\avatar_3.jpg', 1),
 (4, 'Sapo', 'Pepe', 1234567, '121212121', 1, 'elSapo@pepe.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '/Uploads\\avatar_4.jpg', 1),
 (19, 'sanches', 'leandro', 53535355, '123456789', 1, 'leo@sa.com', '3A0G2+zJ3luLnlC44+Xe5HGw/9RWJNoyF2XZACvev20=', '/Uploads\\avatar_0.JPG', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `__efmigrationshistory`
+--
+
+CREATE TABLE `__efmigrationshistory` (
+  `MigrationId` varchar(150) NOT NULL,
+  `ProductVersion` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
@@ -391,6 +449,12 @@ ALTER TABLE `usuario`
   ADD PRIMARY KEY (`Id_Usuario`);
 
 --
+-- Indices de la tabla `__efmigrationshistory`
+--
+ALTER TABLE `__efmigrationshistory`
+  ADD PRIMARY KEY (`MigrationId`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -404,7 +468,7 @@ ALTER TABLE `contrato`
 -- AUTO_INCREMENT de la tabla `inmueble`
 --
 ALTER TABLE `inmueble`
-  MODIFY `Id_Inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `Id_Inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT de la tabla `inquilino`
@@ -416,13 +480,13 @@ ALTER TABLE `inquilino`
 -- AUTO_INCREMENT de la tabla `logs`
 --
 ALTER TABLE `logs`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- AUTO_INCREMENT de la tabla `pago`
 --
 ALTER TABLE `pago`
-  MODIFY `Id_Pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `Id_Pago` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=78;
 
 --
 -- AUTO_INCREMENT de la tabla `propietario`
@@ -434,7 +498,7 @@ ALTER TABLE `propietario`
 -- AUTO_INCREMENT de la tabla `tipo_inmueble`
 --
 ALTER TABLE `tipo_inmueble`
-  MODIFY `Id_Tipo_Inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `Id_Tipo_Inmueble` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
